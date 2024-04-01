001364636ee07dd68bc0f0f7567f75a8d4f48aae
Wairimuu5 <innonjue@gmail.com>
2024-04-01 04:47:28 -0700

    donee

--- sort.h
+++ sort.h
@@ -1,8 +1,7 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef _SORT_H_
+#define _SORT_H_
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -18,14 +17,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Provided print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/* Sorting algorithms prototypes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif /* _SORT_H_ */
